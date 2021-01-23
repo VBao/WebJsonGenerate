43b7c78c7a341f233598e71534a0b18f8c1795f0
VBao <csvobao@gmail.com>
2021-01-23 14:04:12 +0700

    "Update number and string

--- flaskProject/Generate.py
+++ flaskProject/Generate.py
@@ -1,5 +1,5 @@
 import random
-
+import rstr
 from datetime import datetime
 
 
@@ -8,30 +8,24 @@ class Generate:
         def number(*args):
             if len(args) == 1:
                 return random.randint(0, args[0])
-            if len(args) == 2:
+            elif len(args) == 2:
                 return random.randint(args[0], args[1])
-
         # Get random number with total single number in that number
-        def number_len(*args):
-            return random.randint(int("1" + "0" * (args - 1)), int("9" * args))
-
+        def number_len(*args): return random.randint(int("1" + "0" * (int(args[0]) - 1)), int("9" * int(args[0])))
+        def array(*args): return random.choice(args[0])
     class string:
         def string(*args):
-            if len(args) == 1:
-                rs = ""
-                for _ in range(len):
-                    rand = [random.randint(65, 90), random.randint(48, 57),random.randint(79,122)]
-                    func = random.choice(rand)
-                    rs += chr(func)
-                return rs
+            if len(args) == 1: return rstr.xeger(r'^[a-zA-Z]{'+str(args[0])+r'}$')
             if len(args) == 2:
                 rs = ""
                 for _ in range(random.randint(args[0], args[1])):
-                    rand = [random.randint(65, 90), random.randint(48, 57)]
+                    rand = [random.randint(65, 90), random.randint(49, 57)]
                     func = random.choice(rand)
                     rs += chr(func)
                 return rs
+        def array(*args): return random.choice(args[0])
 
+    # TODO continue update datetime
     class datetime:
         def date(*args):
             if len(args) == 1:
@@ -42,9 +36,3 @@ class Generate:
             elif len(args) == 2:
                 start = datetime.strptime(args[0], "%d/%m/%Y")
                 end = datetime.strptime(args[1], "%d/%m/%Y")
-                # for i in range(start, end):
-                #     print(i)
-
-
-a = Generate()
-print(a.datetime.date("20/10/2001","20/12/2001"))
--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,32 +1,54 @@
-from flask import Flask
+from flask import Flask,request
 from flask_api import status
 from Generate import Generate
 import re
-gen=Generate
+import json
 app = Flask(__name__)
 
-def res(key,value):
-    return {key:value}
+
 @app.route('/')
 def hello_world():
     return {"Here":"'Hello World!'"}
 
-@app.route("/<string:nameField>/<string:typeField>/")
-def gen(nameField,typeField):
-    if typeField.find('number') != -1:
-        # return {nameField:str(Generate.number.number(10))},status.HTTP_200_OK
-        # if typeField.find('number-(['):
-        if 'number-([' in typeField:
-            return res("Error","On next update"),status.HTTP_200_OK
-        elif 'number-(' in typeField:
+def strToFunc(typeField):
+    if 'number' in typeField != -1:
+        if '([' in typeField:
+            temp=str(re.findall(r"\[.*\]",typeField)[0])
+            temp=temp[1:len(temp)-1]
+            temp=temp.split(',')
+            return Generate.number.array(temp)
+        elif '(' in typeField:
             start=int(re.findall('\d+',typeField)[0])
             end=int(re.findall('\d+',typeField)[1])
-            return res(nameField,Generate.number.number(start,end)),status.HTTP_200_OK
-        elif 'number-' in typeField:
-            print("ADADW")
-            return res(nameField,Generate.number.number(int(re.findall('\d+',typeField)[0]))),status.HTTP_200_OK
+            return Generate.number.number(start,end)
+        elif '-' in typeField:
+            return Generate.number.number(int(re.findall('\d+',typeField)[0]))
+    elif 'string' in typeField:
+        if '([' in typeField:
+            temp=str(re.findall(r"\[.*\]",typeField)[0])
+            temp=temp[1:len(temp)-1]
+            temp=temp.split(',')
+            print(temp)
+            print(Generate.string.array(temp))
+            return Generate.string.array(temp)
+        elif '(' in typeField:
+            return Generate.string.string(int(re.findall('\d+',typeField)[0]),int(re.findall('\d+',typeField)[1]))
+        elif '-':
+            return  Generate.string.string(int(re.findall('\d+',typeField)[0]))
+    # TODO Datetime here
     else:
         print("NOT UP")
         return {},status.HTTP_403_FORBIDDEN
+
+@app.route('/<int:loop>/',methods=['POST'])
+def convert(loop:int):
+    rq=request.json
+    rs=[]
+    for _ in range(loop):
+        singleRecord={}
+        for i in rq:
+            singleRecord[i]=strToFunc(rq[i])
+        rs.append(singleRecord)
+    return {"result":rs},status.HTTP_200_OK
 if __name__ == '__main__':
     app.run()
