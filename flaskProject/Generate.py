ee6c64e4bbe0a432822b992fa0a0741c6992f185
Biu <csvobao@gmail.com>
2021-01-30 23:19:37 +0700

    update date, datetime and people

--- flaskProject/Generate.py
+++ flaskProject/Generate.py
@@ -1,20 +1,19 @@
 import random
+from time import strftime
 import rstr
 from datetime import *
 
 
 class Generate:
+    def array(*args): return random.choice(args[0])
     class number:
         def number(*args):
             if len(args) == 1:
                 return random.randint(0, args[0])
             elif len(args) == 2:
                 return random.randint(args[0], args[1])
-        # Get random number with total single number in that number
         def number_len(*args): return random.randint(int("1" +
                                                          "0" * (int(args[0]) - 1)), int("9" * int(args[0])))
-
-        def array(*args): return random.choice(args[0])
 
     class string:
         def string(*args):
@@ -28,26 +27,77 @@ class Generate:
                     rs += chr(func)
                 return rs
 
-        def array(*args): return random.choice(args[0])
-
     # TODO continue update datetime
     class datetime:
         def date(*args):
-            if len(args) == 1:
+            if len(args) == 0:
                 year = random.randint(1975, date.today().year)
                 month = random.randint(1, 12)
                 if month in [1, 3, 5, 7, 9, 11]: day = random.randint(1, 31)
                 elif month in [4, 6, 8, 10, 12]: day = random.randint(1, 30)
                 else: day = random.randint(1, 28) if year % 4 == 0 and year % 400 == 0 else random.randint(1, 29)
-                return str(day+'/'+month+'/'+year)
+                day= '0'+str(day) if day<10 else str(day)
+                month= '0'+str(month) if month<10 else str(month)
+                return str(day+'/'+month+'/'+str(year))
             elif len(args) == 2:
                 startDate = datetime.strptime(args[0], "%d/%m/%Y")
                 endDate = datetime.strptime(args[1], "%d/%m/%Y")
                 year = random.randint(startDate.year, endDate.year)
+                month,day=0,0
                 if year == startDate.year:
                     month=random.randint(startDate.month,12)
                     if month !=2 and month %2==0:
                         day=random.randint(startDate.day,30)
                     elif month ==2:
                         day=random.randint(1,29)
-                # elif year
+                day= '0'+str(day) if day<10 else str(day)
+                month= '0'+str(month) if day<10 else str(month)
+                return str(day+'/'+month+'/'+str(year))
+        def datetime(*args):
+            if len(args)==0:
+                year = random.randint(1975, date.today().year)
+                month = random.randint(1, 12)
+                if month in [1, 3, 5, 7, 9, 11]: day = random.randint(1, 31)
+                elif month in [4, 6, 8, 10, 12]: day = random.randint(1, 30)
+                else: day = random.randint(1, 28) if year % 4 == 0 and year % 400 == 0 else random.randint(1, 29)
+                tempDate= datetime(year,month,day).strftime("%a %b %d %Y ")
+                h=random.randint(0,24)
+                h='0'+str(h) if h<10 else str(h)
+                m=random.randint(0,60)
+                m='0'+str(m) if m<10 else str(m)
+                s=random.randint(0,60)
+                s='0'+str(s) if s<10 else str(s)
+                tempTime= h+":"+ m+":"+s
+                return tempDate +tempTime
+            elif len(args) == 2:
+                startDate = datetime.strptime(args[0], "%d/%m/%Y")
+                endDate = datetime.strptime(args[1], "%d/%m/%Y")
+                year = random.randint(startDate.year, endDate.year)
+                month,day=0,0
+                if year == startDate.year:
+                    month=random.randint(startDate.month,12)
+                    if month !=2 and month %2==0:
+                        day=random.randint(startDate.day,30)
+                    elif month ==2:
+                        day=random.randint(1,29)
+                day= '0'+str(day) if day<10 else str(day)
+                month= '0'+str(month) if day<10 else str(month)
+                tempDate= strftime(datetime.datetime(year,month,day),"%a %b %d %Y ")
+                tempTime= str(random.randint(0,24))+":"+ str(random.randint(0,60))+":"+str(random.randint(0,60))
+                return tempDate +tempTime
+    class people:
+        def name():
+            l=open('../Dictionary/lastname.txt','r+').readlines()
+            f=open('../Dictionary/firstname.txt','r+').readlines()
+            return str(random.choice(l).strip('\n')+' '+ random.choice(f).strip('\n'))
+        def gender(*arsg):
+            if len(arsg) ==0:
+                return random.choice(['male','female'])
+            else:
+                return random.choice(arsg[0],arsg[1])
+        def tel(*args):
+            return ('0' if len(args) ==0 else args[0])+str(random.randint(100000000,999999999))
+        def add():
+            l=open('../Dictionary/lastname.txt','r+').readlines()
+            f=open('../Dictionary/firstname.txt','r+').readlines()
+            return str(random.randint(1,500))+ ' '+ str(random.choice(l).strip('\n')+' '+ random.choice(f).strip('\n'))
--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,73 +1,104 @@
-from flask import Flask,request,jsonify
+from datetime import date, datetime
+from flask import Flask, request, jsonify
 from flask_api import status
 from Generate import Generate
 from flask_cors import CORS, cross_origin
 import re
 import json
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r'/test':{"origins": "http: // localhost: 3000"}})
 
+
+def decode(typeFields):
+    if 'custom-{' in typeFields:
+        temp=typeFields.split("{")[1].strip("} ").split(';')
+        rs=""
+        for i in temp:
+            i=i.strip()
+            rs+=strToFunc(i)
+        return rs
+    else: 
+        return strToFunc(typeFields)
 def strToFunc(typeField):
     if 'number' in typeField != -1:
         if '([' in typeField:
-            temp=str(re.findall(r"\[.*\]",typeField)[0])
-            temp=temp[1:len(temp)-1]
-            temp=temp.split(',')
-            return Generate.number.array(temp)
+            temp = str(re.findall(r"\[.*\]", typeField)[0])
+            temp = temp[1:len(temp)-1]
+            temp = temp.split(',')
+            return Generate.array(temp)
         elif '(' in typeField:
-            start=int(re.findall('\d+',typeField)[0])
-            end=int(re.findall('\d+',typeField)[1])
-            return Generate.number.number(start,end)
+            start = int(re.findall('\d+', typeField)[0])
+            end = int(re.findall('\d+', typeField)[1])
+            return Generate.number.number_len(start, end)
         elif '-' in typeField:
-            return Generate.number.number(int(re.findall('\d+',typeField)[0]))
+            return Generate.number.number_len(int(re.findall('\d+', typeField)[0]))
     elif 'string' in typeField:
+        if '([' in typeField:
+            temp = str(re.findall(r"\[.*\]", typeField)[0])
+            temp = temp[1:len(temp)-1]
+            temp = temp.split(',')
+            return Generate.array(temp)
+        elif '(' in typeField:
+            return Generate.string.string(int(re.findall('\d+', typeField)[0]), int(re.findall('\d+', typeField)[1]))
+        elif '-':
+            return Generate.string.string(int(re.findall('\d+', typeField)[0]))
+    elif 'date' in typeField and 'time' not in typeField:
         if '([' in typeField:
             temp=str(re.findall(r"\[.*\]",typeField)[0])
             temp=temp[1:len(temp)-1]
             temp=temp.split(',')
-            return Generate.string.array(temp)
-        elif '(' in typeField:
-            return Generate.string.string(int(re.findall('\d+',typeField)[0]),int(re.findall('\d+',typeField)[1]))
-        elif '-':
-            return  Generate.string.string(int(re.findall('\d+',typeField)[0]))
-    # TODO Datetime here
+            return Generate.array(temp)
+        elif '(' in typeField: 
+            temp=typeField.split('-(')[1].strip(')')
+            temp=temp.split(',')
+            return Generate.datetime.date(temp)
+        else:
+            return Generate.datetime.date()
+    elif 'datetime' in typeField:
+        if '([' in typeField:
+            temp=str(re.findall(r"\[.*\]",typeField)[0])
+            temp=temp[1:len(temp)-1]
+            temp=temp.split(',')
+            return Generate.array(temp)
+        elif '(' in typeField: 
+            temp=typeField.split('-(')[1].strip(')')
+            temp=temp.split(',')
+            return Generate.datetime.datetime(temp)
+        else:
+            return Generate.datetime.datetime()
+    elif 'name' in typeField: return Generate.people.name()
+    elif 'gender' in typeField:
+        temp=typeField.split('-')
+        return Generate.people.gender(temp[1],temp[2])
+    elif 'phone' in typeField:
+            return Generate.people.tel(typeField.split('-')[1]) if '-' in typeField else Generate.people.tel()
+    elif 'address' in typeField: return Generate.people.add()
     else:
         return False
 
-# @app.route('/<int:loop>/',methods=['POST'])
-# def convert(loop:int):
-#     rq=request.json
-#     rs=[]
-#     for _ in range(loop):
-#         singleRecord={}
-#         for i in rq:
-#             if strToFunc(rq[i]) != False:
-#                 singleRecord[i]=strToFunc(rq[i])
-#                 return {"Error":"Invalid type or not update"},status.HTTP_400_BAD_REQUEST
-#         rs.append(singleRecord)
-#     return {"result":rs},status.HTTP_200_OK
 
-@app.route('/test/',methods=['POST'])
-@cross_origin()
+@app.route('/test/', methods=['POST'])
+@cross_origin(origin='localhost')
 def convert():
-    rq=request.json
+    rq = request.json
     print(rq['request'])
     print(rq['total'])
-    loop=rq['total']
-    rquest=rq['request']
-    rs=[]
+    loop = rq['total']
+    rquest = rq['request']
+    rs = []
     for _ in range(loop):
-        singleRecord={}
+        singleRecord = {}
         for i in rquest:
             print(i)
-            if strToFunc(i['type']) != False:
-                singleRecord[i['nameField']]=strToFunc(i['type'])
-            else:    
-                return {"Error":"Invalid type or not update"+str()},status.HTTP_400_BAD_REQUEST
+            # if strToFunc(i['type']) != False:
+            singleRecord[i['nameField']] = decode(i['type'])
+            # else:
+            #     return {"Error": "Invalid type or not update"+str()}, status.HTTP_400_BAD_REQUEST
         rs.append(singleRecord)
-        data=jsonify(rs)
-        data.headers.add("Access-Control-Allow-Origin", "*")
-    return data,status.HTTP_200_OK
+        data = jsonify(data=rs)
+
+    return data, status.HTTP_200_OK
+
+
 if __name__ == '__main__':
     app.run()
